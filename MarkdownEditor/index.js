87bea892c06e7d6b5bb3ec5edbf32c6419271152
Vivian Cermeno <viviancermeno@Vivians-MacBook-Air.local>
2020-10-13 11:24:32 -0700

    feat: Add markdown editor

--- MarkdownEditor/index.js
+++ MarkdownEditor/index.js
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import Editor from 'rich-markdown-editor';
+import path from 'path';
 
 import css from './style.css';
 
 function MarkdownEditor({ file, write }) {
-  console.log(file, write);
+  const [value, setValue] = useState('');
+
+  const handleChange = (value) => {
+      const newFile = new File([value], file.name, {
+        type: file.type,
+        lastModified: Date.now()
+      });
+      console.log(newFile)
+      write(newFile);
+  }
+
+  useEffect(() => {
+    (async () => {
+      const text = await file.text();
+      setValue(text);
+    })();
+  }, [file]);
+
   return (
     <div className={css.editor}>
-      <h3>TODO</h3>
-      <i>text/markdown</i>
+      <div className={css.title}>{path.basename(file.name)}</div>
+      <Editor className={css.content} value={value} onChange={handleChange} />
     </div>
   );
 }
